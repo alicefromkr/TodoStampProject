ddef42f245e026d2c152b94cfbf2bd5d284d450c
choi seoin <inseo@uupfi.com>
2024-01-05 23:21:17 +0900

    typeorm, sqlite setting

--- todo-stamp-api/src/app.module.ts
+++ todo-stamp-api/src/app.module.ts
@@ -2,9 +2,19 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [UserModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'todo-stamp.sqlite',
+      entities: [],
+      synchronize: true, // 개발용, 운영에서는 false로 설정 필요
+      logging: true,
+    }),
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
